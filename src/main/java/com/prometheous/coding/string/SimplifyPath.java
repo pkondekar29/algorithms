fe2222a81275b1427759b3d81818f5af04989d11
Palash Kondekar <palash.kondekar@sap.com>
2021-01-27 13:13:03 +0530

    Strings & arrays

---
+++ src/main/java/com/prometheous/coding/string/SimplifyPath.java
@@ -0,0 +1,61 @@
+package com.prometheous.coding.string;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class SimplifyPath {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(simplify(sc.next()));
+        sc.close();
+    }
+
+    public static String simplify(String path) {
+        Stack<String> sc = new Stack<>();
+        int i = 0;
+        String curr = "";
+        while(i < path.length()) {
+            curr = "";
+            while(i < path.length() && path.charAt(i) == '/') {
+                i++;
+                continue;
+            }
+
+            while(i < path.length() && path.charAt(i) != '/') {
+                curr += path.charAt(i);
+                i++;
+            }
+
+            if(curr.equals("/") || curr.equals(".") || curr.equals("")) {
+                continue;
+            } else {
+                sc.push(curr);
+            }
+        }
+        Stack<String> reversed = new Stack<>();
+        int count = 0;
+        while(!sc.empty()) {
+            curr = sc.pop();
+            if(curr.equals("..")) {     // Pop from stack
+                count++;
+            } else {
+                if(count != 0) {
+                    count--;
+                } else {
+                    reversed.push(curr);
+                }
+            }
+        }
+        if(reversed.isEmpty()) {
+            return "/";
+        }
+        StringBuilder sb = new StringBuilder();
+        while(!reversed.isEmpty()) {
+            sb.append("/").append(reversed.pop());
+        }
+        return sb.toString();
+    }
+
+
+}
---
+++ src/test/java/com/prometheous/coding/string/StrStrTest.java
@@ -0,0 +1,24 @@
+package com.prometheous.coding.string;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class StrStrTest {
+
+    @ParameterizedTest
+    @CsvSource({
+            "Hello, ll, 2",
+            "Wassup, February, -1",
+            "Modelpondel, elpo, 3",
+            " \"\", \"\", 0"
+    })
+    public void test(String s1, String s2, int index) {
+        assertEquals(index, StrStr.strstr(s1, s2));
+    }
+
+}
