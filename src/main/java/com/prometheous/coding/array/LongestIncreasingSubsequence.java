7ee49e85026987a81e525a75882f80a05f341481
Palash Kondekar <palash.kondekar@sap.com>
2021-03-22 10:35:41 +0530

    LCS

---
+++ src/main/java/com/prometheous/coding/array/LongestIncreasingSubsequence.java
@@ -0,0 +1,14 @@
+package com.prometheous.coding.array;
+
+public class LongestIncreasingSubsequence {
+
+    public static void main(String[] args) {
+        System.out.println(findLIS(new int[]{10,9,2,5,3,7,101,18}));
+    }
+
+    private static int findLIS(int[] nums) {
+        int[][] dp = new int[nums.length + 1][nums.length + 1];
+        return 0;
+    }
+
+}
--- src/main/java/com/prometheous/coding/string/LongestCommonSubsequence.java
+++ src/main/java/com/prometheous/coding/string/LongestCommonSubsequence.java
@@ -2,12 +2,16 @@ package com.prometheous.coding.string;
 
 public class LongestCommonSubsequence {
 
+    static int[][] dp;
     public static void main(String[] args) {
-        System.out.println(findLongestCommonSubsequence("abc", "def"));
+        String s1 = "adebcfhg";
+        String s2 = "defg";
+        System.out.println(findLongestCommonSubsequence(s1, s2));
+        System.out.println(findLCS(s1, s2, 8, 4));
     }
 
     private static int findLongestCommonSubsequence(String s1, String s2) {
-        int[][] dp = new int[s1.length() + 1][s2.length() + 1];
+        dp = new int[s1.length() + 1][s2.length() + 1];
         for(int i = 0; i <= s1.length(); i++) {
             dp[i][0] = 0;
         }
@@ -25,6 +29,16 @@ public class LongestCommonSubsequence {
             }
         }
         return dp[s1.length()][s2.length()];
+    }
+
+    private static String findLCS(String s1, String s2, int i, int j) {
+        if(i == 0 || j == 0)
+            return "";
+        if(s1.charAt(i - 1) == s2.charAt(j - 1))
+            return findLCS(s1, s2, i - 1, j - 1) + s1.charAt(i - 1);
+
+        if(dp[i - 1][j] > dp[i][j - 1]) return findLCS(s1, s2, i - 1, j);
+        else return findLCS(s1, s2, i, j - 1);
     }
 
 }
