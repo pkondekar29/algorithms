d50629d24586d4bc27c83e5f0fc67b675c0ae268
Palash Kondekar <palash.kondekar@sap.com>
2021-04-20 11:03:51 +0530

    Rotation and Searching

--- src/test/java/com/prometheous/coding/array/SearchIn2DMatrixTest.java
+++ src/test/java/com/prometheous/coding/array/SearchIn2DMatrixTest.java
@@ -13,16 +13,15 @@ public class SearchIn2DMatrixTest {
     static Stream<Arguments> arrayStream() {
         return Stream.of(
                 Arguments.of(new int[][] { {1,3,5,7}, {10,11,16,20}, {23,30,34,60} }, 3, true),
-                Arguments.of(new int[][] { {1,3,5,7}, {10,11,16,20}, {23,30,34,60} }, 13, false),
-                Arguments.of(new int[][] { {1,3,5,7}, {10,11,16,20}, {23,30,34,60} }, 10, true),
-                Arguments.of(new int[][] { {1,3,5,7}, {10,11,16,20}, {23,30,34,60} }, 60, true)
+                Arguments.of(new int[][] { {1,3,5,7,9}, {10,11,16,20,22}, {23,30,34,60,90} }, 7, true),
+                Arguments.of(new int[][] { {1,3,5,7,9}, {10,11,16,20,22}, {23,30,34,60,90} }, 100, false),
+                Arguments.of(new int[][] { {1,3,5,7,9}, {10,11,16,20,22}, {23,30,34,60,90} }, 0, false),
+                Arguments.of(new int[][] { {1,3,5,7}, {10,11,16,20}, {23,30,34,60} }, 11, true),
+                Arguments.of(new int[][] { {1,3,5,7}, {10,11,16,20}, {23,30,34,60} }, 60, true),
+                Arguments.of(new int[][] { {1,3,5,7}, {10,11,16,20}, {23,30,34,60} }, 100, false),
+                Arguments.of(new int[][] { {1,3} }, 3, true),
+                Arguments.of(new int[][] { {1,3} }, 0, false)
             );
-    }
-
-    static Stream<Arguments> arrayStreamSingle() {
-        return Stream.of(
-                Arguments.of(new int[][] { {1,3,5,7}, {10,11,16,20}, {23,30,34,60} }, 13, false)
-        );
     }
 
     @ParameterizedTest
@@ -32,9 +31,8 @@ public class SearchIn2DMatrixTest {
     }
 
     @ParameterizedTest
-    @MethodSource(value = "arrayStreamSingle")
-    public void testSingle(int [][] a, int target, boolean expected) {
-        assertEquals(SearchIn2DMatrix.search(a, target), expected);
+    @MethodSource(value = "arrayStream")
+    public void testB(int [][] a, int target, boolean expected) {
+        assertEquals(SearchIn2DMatrix.bSearch(a, target), expected);
     }
-
 }
