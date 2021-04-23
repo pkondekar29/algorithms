3c04f2ac72c90854cdd1fe820eb0ea438360c061
Palash Kondekar <palash.kondekar@sap.com>
2021-04-23 17:38:31 +0530

    Tree

--- src/main/java/com/prometheous/coding/utils/PrinterUtils.java
+++ src/main/java/com/prometheous/coding/utils/PrinterUtils.java
@@ -10,4 +10,10 @@ public class PrinterUtils {
             System.out.println();
         }
     }
+
+    public static void print(int[] a) {
+        for(int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
+        }
+    }
 }
