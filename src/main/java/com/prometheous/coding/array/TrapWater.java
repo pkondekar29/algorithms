f1cfd216ba9886664bdfb2aaee48d0ed1ed47c45
Palash Kondekar <palash.kondekar@sap.com>
2021-05-11 12:43:12 +0530

    DP

--- src/main/java/com/prometheous/coding/array/TrapWater.java
+++ src/main/java/com/prometheous/coding/array/TrapWater.java
@@ -33,23 +33,22 @@ public class TrapWater {
         return totalSum;
     }
 
-    // TODO -> Correct it
     public static int trapEff(int[] a) {
         int totalSum = 0;
         int n = a.length;
-        int left[] = new int[n];
-        int right[] = new int[n];
+        int[] leftMax = new int[n];
+        int[] rightMax = new int[n];
 
-        left[0] = a[0];
+        leftMax[0] = a[0];
         for(int i = 1; i < n; i++)
-            left[i] = Math.max(a[i - 1], a[i]);
+            leftMax[i] = Math.max(leftMax[i - 1], a[i]);
 
-        right[n - 1] = a[n - 1];
+        rightMax[n - 1] = a[n - 1];
         for(int i = n - 2; i >= 0; i--)
-            right[i] = Math.max(a[i + 1], a[i]);
+            rightMax[i] = Math.max(rightMax[i + 1], a[i]);
 
         for(int i = 0; i < n; i++)
-            totalSum += (Math.min(left[i], right[i]) - a[i]);
+            totalSum += (Math.min(leftMax[i], rightMax[i]) - a[i]);
 
         return totalSum;
     }
