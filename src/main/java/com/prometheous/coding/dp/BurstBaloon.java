e9651f1ec6452c7ed99ae0a692dcef3b7178790d
pkondekar29 <pkondekar29@gmail.com>
2025-07-15 18:30:19 +0530

    eff

--- src/main/java/com/prometheous/coding/dp/BurstBaloon.java
+++ src/main/java/com/prometheous/coding/dp/BurstBaloon.java
@@ -40,4 +40,28 @@ public class BurstBaloon {
 
         return res;
     }
+
+    // T = O(N3), S = O(N2)
+    public int burstBaloonEff(int[] nums) {
+        int n = nums.length;
+        int[] arr = new int[n + 2];
+        arr[0] = arr[n + 1] = 1;
+        for(int i = 1; i <= n; i++) {
+            arr[i] = nums[i - 1];
+        }
+        int[][] memo = new int[n + 2][n + 2];
+        return burst(memo, arr, 0, n + 1);
+    }
+
+    private int burst(int[][] memo, int[] arr, int left, int right) {
+        if(left + 1 == right) return 0;
+        if(memo[left][right] > 0) return memo[left][right];
+        int ans = 0;
+        for(int i = left + 1; i < right; ++i) {
+            ans = Math.max(ans,
+                    (arr[left] * arr[i] * arr[right]) + burst(memo, arr, left, i) + burst(memo, arr, i, right));
+        }
+        memo[left][right] = ans;
+        return ans;
+    }
 }
